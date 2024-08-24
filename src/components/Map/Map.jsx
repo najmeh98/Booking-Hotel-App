1181ec14ffa9b90ef8f09e1922d16dda2b850e65
najmeh98 <foroughi.najmeh98@gmail.com>
2024-08-24 17:47:21 +0330

    Add map component

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,70 @@
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useGeoLocation from "../../hooks/useGeoLocation";
+import useUrlLocation from "../../hooks/useUrlLocation";
+
+export default function Map({ markerLocation }) {
+  const [mapCenter, setMapCenter] = useState([51, 3]);
+
+  const [lat, lng] = useUrlLocation();
+
+  const { isLoading: isLoadingPosition, position: geoLocationPosition } =
+    useGeoLocation();
+
+  useEffect(() => {
+    if (lat && lng) setMapCenter([lat, lng]);
+  }, [lat, lng]);
+
+  useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
+
+  return (
+    <div className="mapContainer">
+      <MapContainer
+        className="map"
+        center={[lat || 50, lng || 3]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <button className="getLocation"> Use Your Location</button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <DetectClick />
+        <ChangeCenter position={mapCenter} />
+
+        {markerLocation.map((item) => (
+          <Marker key={item.id} position={[item.latitude, item.longitude]}>
+            <Popup>{item.host_location}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvent({
+    click: (e) =>
+      navigate(`/bookmark/add?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+  return null;
+}
