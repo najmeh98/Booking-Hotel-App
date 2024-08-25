80810699bddf895fef3dd87d08cf56d7c6c198b2
najmeh98 <foroughi.najmeh98@gmail.com>
2024-08-25 11:34:42 +0330

    Add bookmark Layout

---
+++ src/components/BookmarkLayout/BookmarkLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Map from "../Map/Map";
+import { useBookmark } from "../context/BookmarkListContext";
+
+export default function BookmarkLayout() {
+  const { bookmarks } = useBookmark();
+
+  return (
+    <div className="appLayout">
+      <div className="sidebar">
+        <Outlet />
+      </div>
+      <Map markerLocation={bookmarks} />
+    </div>
+  );
+}
