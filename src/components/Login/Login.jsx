d2b0cc4232c8dddbb2ee671b164bdab79f234525
najmeh98 <foroughi.najmeh98@gmail.com>
2024-08-26 16:02:48 +0330

    Add login component

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, isAthenticated } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  };
+
+  useEffect(() => {
+    if (isAthenticated) navigate("/", { replace: true });
+  }, [isAthenticated, navigate]);
+
+  return (
+    <div className="loginContainer">
+      <h2 style={{ color: "lightskyblue" }}>Login</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="formControl">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            id="email"
+          />
+        </div>
+        <div className="formControl">
+          <label htmlFor="email">Password</label>
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            id="password"
+          />
+        </div>
+        <div className="buttons">
+          <button className="btn btn--primary">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
